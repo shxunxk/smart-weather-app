ca58003040119d4bff503a71485fda16118d190b
Shaunak <ssnagvenkar@gmail.com>
2026-05-07 17:10:43 +0530

    Deployment ready

--- weatherapp/src/Pages/MapView.js
+++ weatherapp/src/Pages/MapView.js
@@ -296,7 +296,7 @@ export default function MapView() {
         zoom={5}
         style={{ height: "100vh", width: "100%" }}
       >
-        <TileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png" />
+        <TileLayer url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" />
 
         <MapController
           userLocation={userLocation}
