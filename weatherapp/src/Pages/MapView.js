93f297e02289652194d547e8453f94ad0ba08271
Shaunak <ssnagvenkar@gmail.com>
2026-05-07 17:17:36 +0530

    Deployment ready

--- weatherapp/src/Pages/MapView.js
+++ weatherapp/src/Pages/MapView.js
@@ -296,7 +296,8 @@ export default function MapView() {
         zoom={5}
         style={{ height: "100vh", width: "100%" }}
       >
-        <TileLayer url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" />
+        <TileLayer url="https://{s}.basemaps.cartocdn.com/dark_matter/{z}/{x}/{y}{r}.png"
+          attribution='&copy; OpenStreetMap contributors &copy; CARTO' />
 
         <MapController
           userLocation={userLocation}
